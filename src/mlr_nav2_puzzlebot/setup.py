e0cf0a7bcf1e34732b5159e669f9c6a0433da3f7
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2025-04-30 17:55:26 -0600

    feat(ROS2): Package creation

---
+++ src/mlr_nav2_puzzlebot/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = 'mlr_nav2_puzzlebot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='paolo',
+    maintainer_email='paolo.alfonso.reyes@gmail.com',
+    description='Puzzlebot navigation using nav2',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
