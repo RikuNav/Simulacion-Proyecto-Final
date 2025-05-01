0c10283a55805a6ed7eb85d2707881ef74e88dae
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2025-05-01 12:59:24 -0600

    fix(Structure): file names and code organization

--- src/mlr_nav2_puzzlebot/launch/launch.py
+++ src/mlr_nav2_puzzlebot/launch/launch.py
@@ -1,40 +1,32 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
-
-#IMPORTS REQUIRED TO SET THE PACKAGE ADDRESS (DIRECTORIES)
-import os
-from ament_index_python.packages import get_package_share_directory
-
-#IMPORTS REQUIRED FOR Launching Nodes
 from launch_ros.actions import Node
-
-#IMPORTS REQUIRED FOR EVENTS AND ACTIONS
-from launch.events import Shutdown
 
 
 def generate_launch_description():
+    # Get the package share directory
+    package_share_dir = get_package_share_directory('mlr_nav2_puzzlebot')
 
-    base_path = get_package_share_directory('mlr_nav2_puzzlebot')
+    # Get the path to the node resources
+    world_path = os.path.join(package_share_dir, 'worlds', 'puzzlebot_world.world')
 
+    # Nodes definition
+    gz_process = ExecuteProcess(cmd=['gz', 'server', '-r', world_path],
+                                output='screen',)
 
-    rviz_config = os.path.join(base_path, 'rviz', 'name_rviz.rviz')
+    # rviz_config = os.path.join(base_path, 'rviz', 'name_rviz.rviz')
     
     
-    rviz_node = Node(name='rviz',
-                    package='rviz2',
-                    executable='rviz2',
-                    arguments=['-d', rviz_config],
-                    on_exit=Shutdown(),
-                    )
+    # rviz_node = Node(name='rviz',
+    #                 package='rviz2',
+    #                 executable='rviz2',
+    #                 arguments=['-d', rviz_config],)
     
-    world_path = os.path.join(base_path, 'worlds', 'reto.world')
-    
-    return LaunchDescription([
-        ExecuteProcess(
-            cmd=['gz', 'sim', '-r', world_path],
-            output='screen'
-        ), 
-        rviz_node
-    ]) 
+    l_d = LaunchDescription([gz_process,])
+
+    return l_d
 
 
