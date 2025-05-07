ea1000fb45dc55f32dd1f06fbb83331f34f54452
paolor <paolor@Ubuntu.myguest.virtualbox.org>
2025-05-07 13:13:29 -0600

    fix(gz): Gz garden migration and auto setting of GZ_SIM_RSOURCE_PATH env var

---
+++ ls
@@ -0,0 +1,1 @@
+/home/paolor/Simulacion-Proyecto-Final/install/mlr_nav2_puzzlebot/share/mlr_nav2_puzzlebot/models
--- src/mlr_nav2_puzzlebot/launch/launch.py
+++ src/mlr_nav2_puzzlebot/launch/launch.py
@@ -2,11 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, DeclareLaunchArgument
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 
 def generate_launch_description():
     # Get the package share directory
@@ -15,85 +11,49 @@ def generate_launch_description():
     # Get the path to the node resources
     world_path = os.path.join(package_share_dir, 'worlds', 'puzzlebot_world.world')
 
-    # Robot Description
-    robot_file = 'puzzlebot.urdf'
-    urdf = os.path.join(
-        package_share_dir, 
-        'urdf', 
-        robot_file
-    )
-
-    with open(urdf, 'r') as infp:
-        robot_description = infp.read()
-
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'robot_description': robot_description,
-        }],
-        arguments=[urdf],
-    )
-
-    world_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments= [
-            '--x', '0.0', '--y', '0.0', '--z', '0.0',
-            '--roll', '0.0', '--pitch', '0.0', '--yaw', '0.0',
-            '--frame-id', 'map', '--child-frame-id', 'odom']
-    )
-
     # Nodes definition
     gz_process = ExecuteProcess(cmd=['gz', 'sim', world_path],
                                 output='screen',)
 
     gz_service_spawn_puzzlebot = ExecuteProcess(cmd=['gz', 'service', '-s', '/world/default/create', 
-                                                                        '--reqtype', 'gz.msgs.EntityFactory',
-                                                                        '--reptype', 'gz.msgs.Boolean',
-                                                                        '--timeout', '1000',
-                                                                        '--req', f'sdf_filename: "file://{package_share_dir}/models/puzzlebot.sdf" name: "puzzlebot"'],
-                                output='screen',)
+                                                    '--reqtype', 'gz.msgs.EntityFactory',
+                                                    '--reptype', 'gz.msgs.Boolean',
+                                                    '--timeout', '1000',
+                                                    '--req', f'sdf_filename: "file://{package_share_dir}/models/puzzlebot.sdf" name: "puzzlebot"'],
+                                                output='screen',)
     
-    joint_state_publisher_node = Node(
-        package='mlr_nav2_puzzlebot',
-        executable='movement',
-        output='screen',
+    # Set Gazebo environment variables
+    set_gazebo_resources = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=os.path.dirname(package_share_dir)
     )
     
-    # Declare argument
-    declare_rviz_arg = DeclareLaunchArgument(
-        'rviz_config_file',
-        default_value='nav.rviz',
-        description='RViz config file name to load'
-    )
+    # # Declare argument
+    # declare_rviz_arg = DeclareLaunchArgument(
+    #     'rviz_config_file',
+    #     default_value='nav.rviz',
+    #     description='RViz config file name to load'
+    # )
 
-    # Define the RViz node with LaunchConfiguration directly for the file path
-    rviz_config_file = LaunchConfiguration('rviz_config_file')
-    rviz_config_path = PathJoinSubstitution([
-        FindPackageShare('mlr_nav2_puzzlebot'),
-        'rviz_config',
-        LaunchConfiguration('rviz_config_file')
-    ])
+    # # Define the RViz node with LaunchConfiguration directly for the file path
+    # rviz_config_file = LaunchConfiguration('rviz_config_file')
+    # rviz_config_path = PathJoinSubstitution([
+    #     FindPackageShare('mlr_nav2_puzzlebot'),
+    #     'rviz_config',
+    #     LaunchConfiguration('rviz_config_file')
+    # ])
 
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', rviz_config_path]
-    )
+    # rviz_node = Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     name='rviz2',
+    #     output='screen',
+    #     arguments=['-d', rviz_config_path]
+    # )
     
-    l_d = LaunchDescription([gz_process,
-                            gz_service_spawn_puzzlebot,
-                            robot_state_publisher_node,
-                            world_node,
-                            joint_state_publisher_node,
-                            declare_rviz_arg,
-                            rviz_node,
-                            ])
+    l_d = LaunchDescription([set_gazebo_resources,
+                            gz_process,
+                            gz_service_spawn_puzzlebot,])
 
     return l_d
 
