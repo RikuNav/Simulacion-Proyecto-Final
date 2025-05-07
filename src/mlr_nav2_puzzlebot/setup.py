663d70fdbba21018f194663bc95cfec3b7c62db7
AlainVA <89211436+AVA758@users.noreply.github.com>
2025-05-07 16:57:24 -0600

    Add files via upload

--- src/mlr_nav2_puzzlebot/setup.py
+++ src/mlr_nav2_puzzlebot/setup.py
@@ -13,9 +13,31 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
-        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.world'))),
-    ],
+
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+    ]+ [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('urdf') for file in files
+    ]+
+    [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('meshes') for file in files
+    ]+
+    [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('models') for file in files
+    ]+
+    [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('worlds') for file in files
+    ]
+    +
+    [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('plugins') for file in files
+    ],  
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Paolo Reyes | Ricardo Navarro | Fernando Matute | Alain Vicencio',
@@ -24,6 +46,8 @@ setup(
     license='MIT',
     entry_points={
         'console_scripts': [
+            'movement = mlr_nav2_puzzlebot.movement:main',
+            'puzzlebot_gazebo = mlr_nav2_puzzlebot.puzzlebot_gazebo:main',
         ],
     },
 )
