dcd4ee209240ea8439d07677d1784da941d867e5
AlainVA <89211436+AVA758@users.noreply.github.com>
2025-05-07 17:01:30 -0600

    Add files via upload

--- src/mlr_nav2_puzzlebot/setup.py
+++ src/mlr_nav2_puzzlebot/setup.py
@@ -13,14 +13,31 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
-        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.world'))),
-        (os.path.join('share', package_name, 'models'), glob(os.path.join('models', '*.sdf'))),
-        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.stl'))),
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
+
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
         (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
-        ('share/' + package_name + '/rviz_config', ['rviz_config/nav.rviz']),
-    ],
+    ]+ [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('urdf') for file in files
+    ]+
+    [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('meshes') for file in files
+    ]+
+    [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('models') for file in files
+    ]+
+    [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('worlds') for file in files
+    ]
+    +
+    [
+        (os.path.join('share', package_name, root), [os.path.join(root, file)]) 
+        for root, _, files in os.walk('plugins') for file in files
+    ],  
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Paolo Reyes | Ricardo Navarro | Fernando Matute | Alain Vicencio',
@@ -30,6 +47,7 @@ setup(
     entry_points={
         'console_scripts': [
             'movement = mlr_nav2_puzzlebot.movement:main',
+            'puzzlebot_gazebo = mlr_nav2_puzzlebot.puzzlebot_gazebo:main',
         ],
     },
 )
