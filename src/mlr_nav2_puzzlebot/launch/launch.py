4511b4a9466949049316fd6d6e974d4e21847a2d
paolor <paolor@Ubuntu.myguest.virtualbox.org>
2025-05-10 00:31:57 -0600

    fix(gz): refacted launch and auto focus on gz

--- src/mlr_nav2_puzzlebot/launch/launch.py
+++ src/mlr_nav2_puzzlebot/launch/launch.py
@@ -1,4 +1,5 @@
 import os
+import transforms3d as t3d
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -11,51 +12,46 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     # Node variables
+    map_filename = 'puzzlebot_map.yaml'
+    param_filename = 'nav2_config.yaml'
+    world_filename = 'puzzlebot_world.world'
+    robot_xacro_filename = 'puzzlebot.xacro'
+    ros_gz_bridge_config_filename = 'puzzlebot_bridge.yaml'
+
+    initial_x = -1.2
+    initial_y = 1.2
+    initial_theta = -1.57
+
+    # Node parameters default values
     sim_time = 'true'
-    # Robot's initial position
-    pos_x = '0.0'
-    pos_y = '0.0'
-    pos_th = '0.0'
     op_mode = 'map'
-    param_file_name = 'nav2_config.yaml'
 
     # Launch arguments
     declare_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value=sim_time, description='Use simulated time')
-    declare_x_arg = DeclareLaunchArgument('x', default_value=pos_x, description='X position of the robot')
-    declare_y_arg = DeclareLaunchArgument('y', default_value=pos_y, description='Y position of the robot')
-    declare_theta_arg = DeclareLaunchArgument('theta', default_value=pos_th, description='angle of the robot')
     declare_mode_arg = DeclareLaunchArgument('mode', default_value=op_mode, description='Mode of operation (map or nav)')
-    declare_map_dir = DeclareLaunchArgument('map', 
-                                    default_value=
-                                    os.path.join(
-                                        get_package_share_directory('mlr_nav2_puzzlebot'),
-                                        'maps',
-                                        'puzzlebot_map.yaml'),
-                                    description='Full path to map file to load')
-    declare_param_file_name = DeclareLaunchArgument('param_file_name',
-                                                    default_value=os.path.join(
-                                                        get_package_share_directory('mlr_nav2_puzzlebot'),
-                                                        'config',
-                                                        param_file_name),
-                                                    )
 
+    # Get launch params values
     use_sim_time = LaunchConfiguration('use_sim_time')
-    x = LaunchConfiguration('x')
-    y = LaunchConfiguration('y')
-    theta = LaunchConfiguration('theta')
     mode = LaunchConfiguration('mode')
-    map_directory = LaunchConfiguration('map')
-    param_dir = LaunchConfiguration('param_file_name')
 
     # Get the package share directory
     package_share_dir = get_package_share_directory('mlr_nav2_puzzlebot')
-
+    
     # Get the path to the node resources
-    world_path = os.path.join(package_share_dir, 'worlds', 'puzzlebot_world.world')
+    world_path = os.path.join(package_share_dir, 'worlds', world_filename)
 
-    # Paths for world and robot description
-    robot_path = os.path.join(package_share_dir, 'urdf', 'puzzlebot.xacro')
+    # Get the path to map
+    map_path = os.path.join(package_share_dir, 'maps', map_filename)
 
+    # Get the path to nav2 params
+    nav2_params_path = os.path.join(package_share_dir, 'config', param_filename)
+
+    # Get the path to ros_gz_bridge config
+    ros_gz_bridge_config_path = os.path.join(package_share_dir, 'config', ros_gz_bridge_config_filename)
+
+    # Path for robot xacro
+    robot_path = os.path.join(package_share_dir, 'urdf', robot_xacro_filename)
+    
     # Robot description
     robot_description = Command(['xacro ', str(robot_path),
                                 ' camera_frame:=', 'camera_link_optical',
@@ -74,14 +70,16 @@ def generate_launch_description():
     )
 
     # Nodes definition
+    # Static map to odom transform
     map_odom_transform_node = Node(name='map_odom_transform',
                                     package='tf2_ros',
                                     executable='static_transform_publisher',
                                     output='screen',
-                                    arguments=['--x', '1', '--y', '1', '--z', '0', 
+                                    arguments=['--x', '0', '--y', '0', '--z', '0', 
                                                 '--yaw', '0', '--pitch', '0', '--roll', '0', 
                                                 '--frame-id', 'map', '--child-frame-id', 'odom'],)
     
+    # Robot state publisher
     robot_state_publisher_node = Node(package="robot_state_publisher",
                                         executable="robot_state_publisher",
                                         output="screen",
@@ -90,7 +88,8 @@ def generate_launch_description():
                                             "use_sim_time": use_sim_time,
                                         }],)
 
-    gz_process = ExecuteProcess(cmd=['gz', 'sim', world_path, '-v', '4'],
+    # Gz world and puzzlebot launch
+    gz_process = ExecuteProcess(cmd=['gz', 'sim', world_path],
                                 output='screen',)
     
     gz_spawn_puzzlebot_node = Node(package="ros_gz_sim",
@@ -98,19 +97,26 @@ def generate_launch_description():
                                     arguments=[
                                         "-name", "puzzlebot",
                                         "-topic", "robot_description",
-                                        "-x", x, "-y", y, "-Y", theta,
+                                        "-x", str(initial_x), "-y", str(initial_y), "-Y", str(initial_theta),
                                     ],
                                     output="screen",)
 
+    # Change gz camera view
+    q = t3d.euler.euler2quat(0.0, 1.5708, 1.5708)
+    gz_camera_process = ExecuteProcess(cmd=['gz', 'service', '-s', '/gui/move_to/pose', 
+                                            '--reqtype', 'gz.msgs.GUICamera',
+                                            '--reptype', 'gz.msgs.Boolean',
+                                            '--timeout', '10000',
+                                            '--req', f'pose: {{position: {{x: 0.0, y: 0.0, z: 3.0}} orientation: {{x: {q[1]}, y: {q[2]}, z: {q[3]}, w: {q[0]}}}}}'],
+                                output='screen',)
+
     # Bridge ROS topics and Gazebo messages for establishing communication
-    ros_gz_bridge_config_file_path = os.path.join(package_share_dir, 'config', f"puzzlebot_bridge.yaml")
     gz_bridge_node = Node(package='ros_gz_bridge',
                         executable='parameter_bridge',
-                        parameters=[{'config_file': ros_gz_bridge_config_file_path,}],
+                        parameters=[{'config_file': ros_gz_bridge_config_path,}],
                         output='screen')
     
-    # Include the navigation stack and SLAM tool
-    # This will only be activated when the mode is set to "map"
+    # Navigation stack
     navigation_stack_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'navigation_launch.py')
@@ -121,6 +127,7 @@ def generate_launch_description():
         condition=IfCondition(PythonExpression(['"', mode, '" == "map"']))
     )
 
+    # SLAM toolbox
     slam_tool_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')
@@ -130,56 +137,29 @@ def generate_launch_description():
         }.items(),
         condition=IfCondition(PythonExpression(['"', mode, '" == "map"']))
     )
-
+    
     # Include the navigation node
     navigation_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
         ),
         launch_arguments={
-            'map': map_directory,
+            'map': map_path,
             'use_sim_time': use_sim_time,
-            'params_file': param_dir,
+            'params_file': nav2_params_path,
         }.items(),
         condition=IfCondition(PythonExpression(['"', mode, '" == "nav"']))
     )
-
-    # # Declare argument
-    # declare_rviz_arg = DeclareLaunchArgument(
-    #     'rviz_config_file',
-    #     default_value='nav.rviz',
-    #     description='RViz config file name to load'
-    # )
-
-    # # Define the RViz node with LaunchConfiguration directly for the file path
-    # rviz_config_file = LaunchConfiguration('rviz_config_file')
-    # rviz_config_path = PathJoinSubstitution([
-    #     FindPackageShare('mlr_nav2_puzzlebot'),
-    #     'rviz_config',
-    #     LaunchConfiguration('rviz_config_file')
-    # ])
-
-    # rviz_node = Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     name='rviz2',
-    #     output='screen',
-    #     arguments=['-d', rviz_config_path]
-    # )
     
     l_d = LaunchDescription([declare_sim_time_arg,
-                            declare_x_arg,
-                            declare_y_arg,
-                            declare_theta_arg,
                             declare_mode_arg,
-                            declare_map_dir,
-                            declare_param_file_name,
                             set_gazebo_resources,
                             set_gazebo_plugins,
                             map_odom_transform_node,
                             robot_state_publisher_node,
                             gz_process,
                             gz_spawn_puzzlebot_node,
+                            gz_camera_process,
                             gz_bridge_node,
                             navigation_stack_node,
                             slam_tool_node,
